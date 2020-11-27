68b79480580226d2492dd9b9cfde905b56eabcf2
BluberyyPickle <milinddinesh@gmail.com>
2020-11-28 00:56:38 +0530

    python easy task 3

---
+++ python/stackq.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+from queue import Queue
+
+n = int(input('Enter the number of items to add :'))
+
+q = Queue(maxsize = n)
+
+print('Please enter your items separated by lines.')
+for i in range(n):
+	q.put(input())
+print('Appending from queue to stack.')
+
+stack = []
+while q.empty()==False:
+	stack.append(q.get())
+
+print('printing stack')
+
+while stack:
+	print(stack.pop(),end='')
