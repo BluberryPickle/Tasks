427d52db4c04fd58ec18e76924b182489a9a3296
BluberyyPickle <milinddinesh@gmail.com>
2020-12-17 00:12:15 +0530

    Modified files

--- python/chat_app.py
+++ python/chat_app.py
@@ -7,6 +7,7 @@ import base64
 import socket
 from Crypto.Cipher import AES
 from Crypto import Random
+import hmac
 
 password = 'secret'
 
@@ -25,24 +26,29 @@ def encrypt(plaintext, password):
 	return (cipher,salt,iv)
 
 def hash_gen(text):
-	hash = hashlib.sha256(text).hexdigest()
+	hash = hashlib.sha512(text).hexdigest()
 	return hash 
 
 host = 'local host'
 port = 5001
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.bind(('',port))
 s.listen(1)
 c,add = s.accept()	
 print('Connection from : ',str(add))
 cipher, salt, iv = encrypt(input('Enter your text here: '),password)
+
+password = b'password'
+hmac_hash = hmac.new(cipher, password, hashlib.sha1)
+#print(hmac_hash.digest())
 c.send(cipher)
 c.send('||'.encode())
 c.send(salt)
 c.send('||'.encode())
 c.send(iv)
+print('sent')
 c.send('||'.encode())
 c.send(hash_gen(cipher).encode())
-c.send('||'.encode())	
+c.send('||'.encode())
+c.send(hmac_hash.digest())	
 c.close()
--- python/client.py
+++ python/client.py
@@ -13,14 +13,10 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(('', port)) 
 
 msg = s.recv(2048)
-li=str(msg).split('||') 
-print(msg) 
-#print(li)
-
-'''
+#print(msg)
 while msg: 
     print(str(msg)) 
     msg = s.recv(2048)
-'''
-print(li)
+
+li=str(msg).split('||') 
 s.close() 
